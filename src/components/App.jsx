b93919c5b0b0c99ec7eaa355cef350e6b26bbb51
crisanlucid <crisan.lucid@yahoo.com>
2020-04-21 00:10:19 +0200

    style: update JSX files -files which render JSX will have .jsx extentions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import HomePage from './HomePage';
 
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return <div className='App'>App Component</div>;
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
   }
 }
 
