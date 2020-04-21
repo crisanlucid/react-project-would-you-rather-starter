c481075f7d011df62b56bb6ca0b8e69fdca2cd5c
crisanlucid <crisan.lucid@yahoo.com>
2020-04-21 12:34:48 +0200

    feat:  Question Container - create 2 links - style component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,27 @@ import { connect } from 'react-redux';
 class HomePage extends Component {
   render() {
     console.log(this.props);
-    return <div> Home Page</div>;
+    return (
+      <div className='question-wrapper center'>
+        <nav className='nav nav--block'>
+          <ul>
+            <li>
+              <button className='btn active'>Link 1</button>
+            </li>
+            <li>
+              <button className='btn '>Link 2</button>
+            </li>
+          </ul>
+        </nav>
+        <ul className='question-list'>
+          {this.props.questionIds.map((id) => (
+            <li key={id}>
+              <div>Question ID: {id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
